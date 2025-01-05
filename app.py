c6e8ab6006996ee367ffcc94fcb5b776c3d74c77
Vitaliy2024GPT <162298526+Vitaliy2024GPT@users.noreply.github.com>
2025-01-05 15:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ logger = logging.getLogger(__name__)
 bot_token = os.environ.get('TELEGRAM_TOKEN')
 
 # Инициализируем бота и диспетчера
-bot = Bot(bot_token)
-application = ApplicationBuilder().token(bot_token).defaults(Defaults(parse_mode="HTML", allow_sending_without_reply=True)).build()
-
 
 class User(db.Model):  # Пример модели
     id = db.Column(db.Integer, primary_key=True)
@@ -34,6 +31,8 @@ class User(db.Model):  # Пример модели
 
 with app.app_context():  # Создаем контекст приложения Flask
     db.create_all()      # Создаем таблицы в базе данных
+
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Обработчик команды /start"""
@@ -48,14 +47,6 @@ async def error_handler(update: Update, context: CallbackContext) -> None:
     except Exception as e:
        logger.error(f"Exception in error handler {e}")
 
-async def initialize_bot():
-    await application.initialize()
-    application.add_handler(CommandHandler("start", start))
-    application.add_error_handler(error_handler)
-
-
-
-asyncio.run(initialize_bot())
 
 
 @app.route('/')
@@ -63,6 +54,11 @@ def index():
     return render_template('index.html')
 
 async def process_update(update:Update):
+    bot = Bot(bot_token)
+    application = ApplicationBuilder().token(bot_token).defaults(Defaults(parse_mode="HTML", allow_sending_without_reply=True)).build()
+    await bot.initialize()
+    application.add_handler(CommandHandler("start", start))
+    application.add_error_handler(error_handler)
     await application.process_update(update)
 
 @app.route('/telegram_webhook', methods=['POST'])
